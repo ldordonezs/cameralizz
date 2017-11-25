d6b8f6fdd73ecf25a785cb2e6d45b0b1d5fee42c
ldordonezs <elpatasdos@gmail.com>
2017-11-25 10:32:19 -0500

    inicial

--- lizz.py
+++ lizz.py
@@ -1,2 +1,22 @@
-print ("loquesea2")
-print ("mas")
+import cv2
+
+cap = cv2.VideoCapture(1)
+
+while(True):
+    ret, frame = cap.read()
+
+    if ret:
+        cv2.imshow('video', frame)
+
+    if cv2.waitKey(1) & 0xFF == 27:
+
+        break
+
+         #cv2.imwrite('captura_img.jpg',frame)
+
+    if cv2.waitKey(1) == ord(' ') :
+            cv2.imwrite('captura_img.jpg',frame)
+
+
+cap.release()
+cv2.destroyAllWindows()
